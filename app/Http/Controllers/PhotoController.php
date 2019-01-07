93ef3b14dda6a904c9780024bbf95c97869a5955
Miller Adulu <milleradulu@gmail.com>
2019-01-08 01:28:53 +0300

    Request more data for photos
    Useful to create a good UI

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -22,7 +22,7 @@ class PhotoController extends Controller
         $photos = json_decode($facebookClient->get('https://graph.facebook.com/'. $user->provider_given_id .'/photos/uploaded', [
             'query' => [
                 'access_token' => $user->token,
-                'fields' => 'images'
+                'fields' => 'images,name,album'
             ],
         ])->getBody());
 
