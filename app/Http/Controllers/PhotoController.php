4bf2a48dbc8dc4ca995c641411e6db77c4447166
Miller Adulu <milleradulu@gmail.com>
2019-01-08 23:36:56 +0300

    Display error message when facebook connection fails
    Tests for a connection to the facebook api using the socket function

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -10,22 +10,43 @@ use Illuminate\Support\Facades\Auth;
 
 class PhotoController extends Controller
 {
+    private $apiHost = 'https://graph.facebook.com';
+
     public function facebookPhotos(Request $request)
     {
-        $user = ProviderCredential::where([
-            ['user_id', Auth::id()],
-            ['provider', 'facebook']
-        ])->first();
+        if($this->isConnected())
+        {
+            $user = ProviderCredential::where([
+                ['user_id', Auth::id()],
+                ['provider', 'facebook']
+            ])->first();
 
-        $facebookClient = new Client;
+            $facebookClient = new Client;
 
-        $photos = json_decode($facebookClient->get('https://graph.facebook.com/'. $user->provider_given_id .'/photos/uploaded', [
-            'query' => [
-                'access_token' => $user->token,
-                'fields' => 'images,name,album'
-            ],
-        ])->getBody());
+            $photos = json_decode($facebookClient->get($this->apiHost . '/'. $user->provider_given_id .'/photos/uploaded', [
+                'query' => [
+                    'access_token' => $user->token,
+                    'fields' => 'images,name,album'
+                ],
+            ])->getBody());
 
-        return view('photos.facebook', ['photos' => $photos->data]);
+            $photoData = $photos->data;
+        } else $photoData = null;
+
+        return view('photos.facebook', ['photos' => $photoData]);
+    }
+
+    public function isConnected()
+    {
+        $connected = @fsockopen($this->apiHost, 80|443);
+
+        if($connected) {
+            $isConnected = true;
+            fclose($connected);
+        } else {
+            $isConnected = false;
+        }
+
+        return $isConnected;
     }
 }
--- resources/views/photos/facebook.blade.php
+++ resources/views/photos/facebook.blade.php
@@ -1,52 +1,64 @@
-@extends('layouts.app')
-
-@section('title', 'Facebook Pictures')
-
+@extends('layouts.app') 
+@section('title', 'Facebook Pictures') 
 @section('content')
 
 <div class="outer-container">
-        <div class="container portfolio-page">
-            <div class="row">
-                <div class="col">
-                    <ul class="breadcrumbs flex align-items-center">
-                        <li><a href="#">Home</a></li>
-                        <li>Portfolio</li>
-                    </ul><!-- .breadcrumbs -->
-                </div><!-- .col -->
-            </div><!-- .row -->
-    
-            <div class="row">
+    <div class="container portfolio-page">
+        <div class="row">
+            <div class="col">
+                <ul class="breadcrumbs flex align-items-center">
+                    <li><a href="/">Home</a></li>
+                    <li>Facebook Photos</li>
+                </ul>
+                <!-- .breadcrumbs -->
+            </div>
+            <!-- .col -->
+    @include('partials.authlinks')
+        </div>
+        <!-- .row -->
 
-                    @if (count($photos) > 0)
-                    @foreach ($photos as $photo)                    
+        @if (isset($photos) && count($photos) > 0)
 
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="portfolio-content">
-                        <figure>
-                            <img src="{{ $photo->images[0]->source }}" alt="">
-                        </figure>
-    
-                        <div class="entry-content flex flex-column align-items-center justify-content-center">
-                            <h3><a>{{ $photo->album->name }}</a></h3>
-    
-                            <ul class="flex flex-wrap justify-content-center">
-                                @if (isset($photo->name))
-                                    <li><a>{{ $photo->name }}</a></li>
-                                @endif
-                            </ul>
-                        </div><!-- .entry-content -->
-                    </div><!-- .portfolio-content -->
-                </div><!-- .col -->
-    
-                @endforeach
-                @endif
+        <div class="row">
 
-            </div><!-- .row -->
-    
-            <div class="scroll-down flex flex-column justify-content-center align-items-center d-none d-lg-block">
-                <span class="arrow-down flex justify-content-center align-items-center"><img src="images/arrow-down.png" alt="arrow"></span>
-                <span class="scroll-text">Scroll Down</span>
-            </div><!-- .scroll-down -->
-        </div><!-- .container -->
-    </div><!-- .outer-container -->
+            @foreach ($photos as $photo)
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="portfolio-content">
+                    <figure>
+                        <img src="{{ $photo->images[0]->source }}" alt="">
+                    </figure>
+
+                    <div class="entry-content flex flex-column align-items-center justify-content-center">
+                        <h3><a>{{ $photo->album->name }}</a></h3>
+
+                        <ul class="flex flex-wrap justify-content-center">
+                            @if (isset($photo->name))
+                            <li><a>{{ $photo->name }}</a></li>
+                            @endif
+                        </ul>
+                    </div>
+                    <!-- .entry-content -->
+                </div>
+                <!-- .portfolio-content -->
+            </div>
+            <!-- .col -->
+
+            @endforeach
+
+        </div>
+        <!-- .row -->
+
+        @endif
+
+        <div class="row">
+            <div class="portfolio-content">
+                Unable to connect to Facebook at this time. Please try again later
+            </div>
+        </div>
+
+    </div>
+    <!-- .container -->
+</div>
+<!-- .outer-container -->
 @endsection
